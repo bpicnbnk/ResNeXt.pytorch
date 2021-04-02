bf767201d3d06cc5e51d4825f13ac062d3d42d6b
bpicnbnk <goomarglelly@gmail.com>
2021-04-02 12:13:00 +0800

    fig font

--- utils/plot_log_c100_bn.py
+++ utils/plot_log_c100_bn.py
@@ -1,7 +1,12 @@
+import matplotlib
 import re
 import matplotlib.pyplot as plt
 import os
 from os.path import join
+from matplotlib.font_manager import FontProperties
+font = FontProperties(fname=r"C:\windows\fonts\simsun.ttc", size=14)
+matplotlib.rcParams['pdf.fonttype'] = 42
+matplotlib.rcParams['ps.fonttype'] = 42
 
 
 def str2other(strlist, other):
@@ -58,22 +63,21 @@ obnloss = str2other(obnloss, float)
 
 plt.figure(f'{nameoend}: test_accuracy vs epochs')
 # plt.title(f'{nameoend}: test_accuracy vs epochs')
-plt.xlabel('epoch')
-plt.ylabel('test_accuracy')
+plt.xlabel('周期（epoch）', fontproperties=font)
+plt.ylabel('测试准确率', fontproperties=font)
 # plt.plot(epochs, bnaccuracy, 'b*')
-plt.plot(bnepochs, bnaccuracy, 'r', label='with BN')
+plt.plot(bnepochs, bnaccuracy, 'r:', label='with BN')
 plt.plot(obnepochs, obnaccuracy, 'y', label='without BN')
 plt.legend()
 plt.savefig(f'./figure/{nameoend}_test_accuracy_BN.png')
 
 plt.figure(f'{onameoend}: test_accuracy vs epochs')
 # plt.title(f'{nameoend}: test_accuracy vs epochs')
-plt.xlabel('epoch')
-plt.ylabel('train_loss')
+plt.xlabel('周期（epoch）', fontproperties=font)
+plt.ylabel('训练损失', fontproperties=font)
 # plt.plot(epochs, bnaccuracy, 'b*')
-plt.plot(bnepochs, bnloss, 'r', label='with BN')
+plt.plot(bnepochs, bnloss, 'r:', label='with BN')
 plt.plot(obnepochs, obnloss, 'y', label='without BN')
 plt.legend()
+plt.show()
 plt.savefig(f'./figure/{onameoend}_train_loss_oBN.png')
-
-
